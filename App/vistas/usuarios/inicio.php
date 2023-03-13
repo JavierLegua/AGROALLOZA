03d22bc769b160ef02653b77d0a50e4171e57fc1
Javier Legua <javierlegua14@gmail.com>
2023-03-13 20:01:17 +0100

    Cambio en modal cambiar contraseña usuarios

--- App/vistas/usuarios/inicio.php
+++ App/vistas/usuarios/inicio.php
@@ -172,7 +172,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalcambio">Cambiar contraseña <?php echo $uruario->rol_idrol ?></h5>
+                        <h5 class="modal-title" id="exampleModalcambio">Cambiar contraseña <?php echo $uruario->nombre ?></h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="limpiarC(<?php echo $uruario->idusuario ?>)"></button>
                     </div>
                     <div class="modal-body container">
